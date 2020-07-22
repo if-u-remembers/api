cdd72c7150cc36b3d32c1a0e6cc67c18f1d51f82
zejia guo <if-u-remember@foxmail.com>
2020-07-22 16:22:10 +0800

    新建分支

--- vue_api/form_data.py
+++ vue_api/form_data.py
@@ -3,13 +3,14 @@
     这个代码是用于：
     获取前端界面传来的数据处理
 """
+
+
 class Datas:
     """
         获取表单数据
     """
     def __init__(self):
         pass
-
 
     def formData(self):
         return '表单data'
